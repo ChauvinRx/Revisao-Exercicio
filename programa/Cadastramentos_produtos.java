f9e8890dc7dcb6e29746151a773c473cbf5ecd60
Jonathas Chauvin <jonathaschauvin@gmail.com>
2024-09-02 19:29:03 -0300

    adicionado funções adicionar produtos e listar produtos

--- programa/Cadastramentos_produtos.java
+++ programa/Cadastramentos_produtos.java
@@ -14,19 +14,40 @@ public class Cadastramentos_produtos {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		while(true) {
 		System.out.println("--- Cadastramento ---");
 		System.out.println("[1] Adicionar novo produto");
 		System.out.println("[2] Listar produtos");
-		System.out.println("[3] Pesquisar produto por nome");
-		System.out.println("[4] Editar informações do produto");
-		System.out.println("[5] Deletar um produto");
+		System.out.println("[6] Sair");
 		int opcao = sc.nextInt();
+		sc.nextLine();
 		
 		switch(opcao) {
 		case 1:
-			
+			System.out.print("Nome do produto: ");
+			String nomeProduto = sc.nextLine();
+			System.out.print("Preço: ");
+			double precoProduto = sc.nextDouble();
+			System.out.print("Quantidade: ");
+			int quantidadeProduto = sc.nextInt();
+			Produtos produto = new Produtos(nomeProduto, precoProduto, quantidadeProduto);
+			produtos.add(produto);
+			break;
+		case 2:
+			System.out.println("[Produtos registrados]");
+			System.out.println(produtos.size() + " itens");
+			for (Produtos prod : produtos) {
+				System.out.println(prod);
+			}
+			break;
+		case 6:
+			System.out.println("Saindo...");
+			sc.close();
+			return;
+		default:
+			System.out.println("Opção inválida!");
 		}
-		sc.close();
+		}
 	}
 	
 
