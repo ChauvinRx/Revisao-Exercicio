108338b97a232a52d4fb527f82617cbed6b7ee55
Jonathas Chauvin <jonathaschauvin@gmail.com>
2024-09-25 23:27:44 -0300

    Funcao atualizar os contatos

--- agenda_contatos/Agenda.java
+++ agenda_contatos/Agenda.java
@@ -31,9 +31,10 @@ public class Agenda {
 			agenda.adicionarContato();
 			break;
 		case 2:
-			System.out.print("Nome do contato que vai remover: ");
-			String nome = sc.nextLine();
-			agenda.removerContato(nome);
+			agenda.removerContato();
+			break;
+		case 3:
+			agenda.atualizarContato();
 			break;
 		default:
 			System.out.println("Opção Inválida!");
@@ -58,15 +59,53 @@ public class Agenda {
 		System.out.println("O contato foi adicionado!");
 	}
 	
-	public void removerContato(String nome) {
+	public void removerContato() {
+		System.out.print("Nome do contato que vai remover: ");
+		String nome = sc.nextLine();
 		for(Contato x : contatos) {
 			if(nome.equalsIgnoreCase(x.getNome())) {
 				contatos.remove(x);
-				System.out.println("O contato " + x + " foi removido");
+				System.out.println("O contato " + x.getNome() + " foi removido");
 			}else {
 				System.out.println("Contato não existente");
 			}
 		}
+	}
+	
+	public void atualizarContato() {
+		System.out.print("Digite o nome do contato que deseja atualizar: ");
+        String nome = sc.nextLine();
+        Contato contato = null;
+        
+		for(Contato x : contatos) {
+			if(nome.equalsIgnoreCase(x.getNome())) {
+				contato = x;
+				break;
+			}
+		}
+		if(contato != null) {
+            // Pergunta os novos dados
+            System.out.print("Novo nome (ou pressione Enter para manter): ");
+            String novoNome = sc.nextLine();
+            if (!novoNome.isEmpty()) {
+                contato.setNome(novoNome);
+            }
+
+            System.out.print("Novo telefone (ou pressione Enter para manter): ");
+            String novoTelefone = sc.nextLine();
+            if (!novoTelefone.isEmpty()) {
+                contato.setTelefone(novoTelefone);
+            }
+
+            System.out.print("Novo email (ou pressione Enter para manter): ");
+            String novoEmail = sc.nextLine();
+            if (!novoEmail.isEmpty()) {
+                contato.setEmail(novoEmail);
+            }
+            System.out.println("Contato atualizado com sucesso!");
+        }else {
+            System.out.println("Contato não encontrado.");
+        }	
 	}
 
 }
